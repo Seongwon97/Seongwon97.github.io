7cb6d30fb6cad9d4c98627c2db2cc113c61d6b96
Seongwon97 <osw970325@naver.com>
2022-07-31 15:32:33 +0900

    블로그 일부 마이그레이션

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `Seongwon97`,
+  description: `[Seongwon's Develop Diary] 열심히 성장하는 중입니다🔥`,
+  author: `[SeongwonOh]`,
+  introduction: `[Seongwon's Develop Diary] 열심히 성장하는 중입니다🔥`,
+  siteUrl: `https://seongwon97.github.io/`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `https://github.com/Seongwon97`, // Your GitHub account
     medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
-    linkedin: ``, // Your LinkedIn account
-    instagram: ``, // Your Instagram account
+    facebook: `https://www.facebook.com/profile.php?id=100004105792447`, // Your Facebook account
+    linkedin: `https://www.linkedin.com/in/%EC%84%B1%EC%9B%90-%EC%98%A4-17797421b/`, // Your LinkedIn account
+    instagram: `https://www.instagram.com/seongwon__97/`, // Your Instagram account
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'Seongwon97/gatsby-starter-bee', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
